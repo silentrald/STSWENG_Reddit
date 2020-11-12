ff2dfcf488a360d5331f9f8b251e9f97aa5f54c3
unknown <silentcommentator1216@gmail.com>
2020-11-12 21:01:22 +0800

    Added migration scripts

---
+++ server/migrations/up.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const fs = require('fs').promises;
+const path = require('path');
+const { Pool } = require('pg');
+
+const client = new Pool({
+    user:       process.env.POSTGRES_USER,
+    password:   process.env.POSTGRES_PASS,
+    host:       process.env.POSTGRES_HOST,
+    port:       process.env.POSTGRES_PORT,
+    database:   process.env.POSTGRES_DB
+});
+
+(async () => {
+    try {
+        const query = await fs.readFile(
+            path.join(__dirname, 'sqls', 'up.sql'),
+            { encoding: 'utf-8' }
+        );
+        await client.query(query);
+        await client.end();
+        console.log('Migration Up Done');
+    } catch (err) {
+        console.log(err);
+    }
+})();
