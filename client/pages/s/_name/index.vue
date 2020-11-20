f1bc6a336518eb86d6ced085406b25c30e5a9c7a
Marc Tiburcio <44109710+marcnt@users.noreply.github.com>
2020-11-20 15:55:28 +0800

    Polish station page

--- client/pages/s/_name/index.vue
+++ client/pages/s/_name/index.vue
@@ -1,28 +1,34 @@
 <template>
   <div>
     <div class="container mt-3">
-      <div class="row">
-        <div id="station-info" class="col-md-3 order-md-2">
-          <div id="station-name-desc">
-            <h3>Welcome to s/{{ this.$route.params.name }}!</h3>
-          </div>
-          <div id="station-rules">
-            <h4>Rules</h4>
-            <div class="box">
-              No rules
+      <div v-if="is404">
+        <h1>Station does not exist</h1>
+      </div>
+      <div v-else-if="hasStation">
+        <div class="row">
+          <div id="station-info" class="col-md-3 order-md-2">
+            <div id="station-name-desc">
+              <h3>Welcome to s/{{ name }}!</h3>
+              {{ description }}
+            </div>
+            <div id="station-rules">
+              <h4>Rules</h4>
+              <div class="box">
+                {{ rules }}
+              </div>
+            </div>
+            <div id="station-mods">
+              <h4>Moderators</h4>
+              <div class="box">
+                c/user
+              </div>
             </div>
           </div>
-          <div id="station-mods">
-            <h4>Moderators</h4>
-            <div class="box">
-              c/user
-            </div>
+          <div id="posts" class="col-md-9 order-md-1">
+            <post />
+            <post />
+            <post />
           </div>
-        </div>
-        <div id="posts" class="col-md-9 order-md-1">
-          <post />
-          <post />
-          <post />
         </div>
       </div>
     </div>
@@ -32,8 +38,42 @@
 <script>
 import post from '../../../components/post.vue'
 export default {
-  components: { post }
+  components: { post },
+  data () {
+    return {
+      is404: false,
+      hasStation: false,
+      name: '',
+      description: '',
+      rules: ''
+    }
+  },
 
+  beforeMount () {
+    this.loadStation()
+  },
+
+  methods: {
+    loadStation () {
+      const name = this.$route.params.name
+      this.$axios.get(`/api/station/id/${name}`)
+        .then((res) => {
+          const { station } = res.data
+          this.$set(this, 'hasStation', true)
+          this.$set(this, 'name', station.name)
+          this.$set(this, 'description', station.description)
+          this.$set(this, 'rules', station.rules)
+        })
+        .catch((err) => {
+          const { status } = err.response
+
+          if (status === 404) {
+            this.$set(this, 'is404', true)
+            // this.errors = customErrors(data.errors, customErrorMsg)
+          }
+        })
+    }
+  }
 }
 </script>
 
