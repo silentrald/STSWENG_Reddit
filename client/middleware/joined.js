eb6e0887a554ab0fdb8d1df81e10a07c023ede9a
Marc Tiburcio <44109710+marcnt@users.noreply.github.com>
2021-01-05 15:44:13 +0800

    Fix 404 on add post

--- client/middleware/joined.js
+++ client/middleware/joined.js
@@ -1,6 +1,6 @@
 export default async function ({ $auth, params, $axios, redirect }) {
   if ($auth.user) {
-    const { name } = params
+    const { station: name } = params
     const station = await $axios.get(`/api/station/id/${name}`)
     const { joined } = station.data
     if (!joined) {
