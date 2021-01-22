b0ced5270a45412bb19bd022578acb3f4493d493
Lance Ricco Teng <47263399+S0rbetes@users.noreply.github.com>
2021-01-22 18:38:44 +0800

    Added tests for getUser

--- server/test/unit/userAPI.test.js
+++ server/test/unit/userAPI.test.js
@@ -4,7 +4,8 @@ const {
     getAuth,
     getUserNames,
     postLogin,
-    postRegisterUser
+    postRegisterUser,
+    getUser
 } = require('../../api/userAPI');
 
 jest.mock('../../db', () => {
@@ -126,6 +127,26 @@ describe('Unit test: userAPI.js', () => {
         });
 
         
+    });
+
+    // TODO: Create unit test for userAPI
+    describe('API: getUser', () => {
+        let params;
+
+        beforeEach(() => {
+            params = {
+                username: 'crewmate'
+            };
+        });
+
+        test('GOOD', async () => {
+            const req = mockRequest({ params });
+            const res = mockResponse();
+            
+            await getUser(req, res);
+
+            expect(res.status).toHaveBeenCalledTimes(1);
+        });
     });
 
     describe('API: postRegisterUser', () => {
